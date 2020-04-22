61c1baac02587eb351882e2f2194d6ba19fb5e1d
annubv <annubv25@gmail.com>
2020-04-22 20:45:33 +0530

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  context: path.resolve(__dirname, "src"),
+  entry: [
+    "./index.js",
+    "./sass/root.scss",
+    "./sass/nav.scss",
+    "./sass/chat-content.scss",
+  ],
+  output: {
+    path: path.resolve(__dirname, "client/assets/css/"),
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /.(scss|css)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MinifyPlugin(
+      {},
+      {
+        comment: false,
+      }
+    ),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
